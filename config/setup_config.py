533881789a1e00f99788b02005f97edad10e2658
tahmee <susantamunokubie@gmail.com>
2025-12-22 19:52:52 +0100

    Update config file

--- config/setup_config.py
+++ config/setup_config.py
@@ -4,19 +4,14 @@ import os
 
 # Setup api_ingest directories
 LOG_DIR = "logs"
-LOG_FILE = "api_ingest.log"
-LOG_PATH = os.path.join(LOG_DIR, LOG_FILE)
 OUTPUT_DIR = "api_data"
-OUTPUT_FILE = "quote_data.json"
-OUTPUT_PATH = os.path.join(OUTPUT_DIR, OUTPUT_FILE)
 
+API_LOG_PATH = os.path.join(LOG_DIR, "api_ingest.log")
+MAIN_LOG_PATH = os.path.join(LOG_DIR, "process.log")
+SUMMARY_LOG_PATH = os.path.join(LOG_DIR, "summary.log")
 
+OUTPUT_PATH = os.path.join(OUTPUT_DIR, "quote_data.json")
 
-# Setup log path and directory
-MAIN_LOG_FILE = "process.log"
-SUMMARY_LOG_FILE = "summary.log"
-M_LOG_PATH = os.path.join(LOG_DIR, MAIN_LOG_FILE)
-SUMMARY_LOG_PATH = os.path.join(LOG_DIR, SUMMARY_LOG_FILE)
 
 class EmailConfig:
     # SMTP credentials/ DB config
@@ -27,11 +22,15 @@ class EmailConfig:
     SMTP_TIMEOUT = int(os.getenv('SMTP_TIMEOUT'))
 
 
-class AppConfig   
+class AppConfig:  
     # Set database credentials and file path
     DB_CREDENTIALS = os.getenv('DB_CREDENTIALS')
     FILE_PATH = os.getenv('FILE_PATH') # Path to quotes file
 
+    # Paths to use in your main scripts
+    LOG_PATH = MAIN_LOG_PATH 
+    SUMMARY_LOG_PATH = SUMMARY_LOG_PATH
+    
     # Alert email configuration
     ALERT_EMAIL = os.getenv('ALERT_EMAIL') 
     SEND_ALERTS = os.getenv('SEND_ALERTS').lower() == 'true'
